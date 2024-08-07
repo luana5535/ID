b62581c81a202df9d8587e12ff663680dcab9e1d
Luana dos Anjos Siqueira <luanasiqueira5535@gmail.com>
2024-08-07 17:06:02 -0300

    Update adicionar.js

--- adicionar.js
+++ adicionar.js
@@ -2,8 +2,18 @@ const usuarios = require("./usuarios")
 
 function adicionar(usuario) {
     usuario.id = usuarios.length +1
-    usuarios.push(usuario)
-    }
 
+    let existe = false;
+    for (let i = 0; i < usuarios.length; i++) {
+        if(usuarios[i].email === usuario.email) {
+        existe = true;
+        console.log('Já existe uma conta vinculada a esse email.')
+        }
+    }   
+        if (!existe) {
+        usuarios.push(usuario);
+        console.log('Usuário cadastrado com sucesso! ')
+        }
+}
 
 module.exports = adicionar 
