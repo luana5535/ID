84410752f8829ff73c440d2c111169a641dbe0f7
Luana dos Anjos Siqueira <luanasiqueira5535@gmail.com>
2024-08-07 17:06:35 -0300

    Update listar.js

--- listar.js
+++ listar.js
@@ -2,9 +2,19 @@ const atualizar = require("./atualizar")
 const usuarios = require("./usuarios")
 
 function listar() {
-    usuarios.forEach((usuario) => {
-        console.log(`id: ${usuario.id}, nome: ${usuario.nome}, email: ${usuario.email}, numero: ${usuario.numero}`)
-    })
-}
+    for (let i = 0; i < usuarios.length; i++) {
+    const usuario = usuarios[i];
+    console.log(`
+    ID: ${usuario.id},
+    Nome: ${usuario.nome},
+    Email: ${usuario.email},
+    `);
+    
+    for (let j = 0; j < usuario.telefones.length; j++) {
+    console.log(` ${j+1}. ${usuario.telefones[j]}`);
+    }
+    console.log('');
+    }
+    }
 
 module.exports = listar
